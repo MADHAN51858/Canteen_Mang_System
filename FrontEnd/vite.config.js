d26243aca7703422aaf83102d2f13702b7a395e0
Madhu <madhanku635@gmail.com>
2026-01-13 11:35:16 +0530

    Update vite config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    preview: {
-    allowedHosts: ["*.onrender.com"],
+   preview: {
+    allowedHosts: true,   // ✅ allow all hosts
+    host: "0.0.0.0",      // ✅ required on Render
   },
 })
